42b5faa78cdf2caaab2c4a1f85099987ec53a65b
AD1337 <lucasgmolina@gmail.com>
2021-01-30 14:20:26 -0300

    drunk shader and withdrawal screenshake done

---
+++ Global Game Jam 2021/Scenes/ScreenShake/Screenshake.gd
@@ -0,0 +1,44 @@
+extends Node
+
+const TRANS = Tween.TRANS_SINE
+const EASE = Tween.EASE_IN_OUT
+
+var amplitude = 0
+
+onready var camera = get_parent()
+
+
+func _ready():
+	start(0,250,1)
+
+
+func start(duration,frequency,_amplitude):
+	self.amplitude = _amplitude
+	$Frequency.wait_time = 1 / float(frequency)
+	if duration > 0:
+		$Duration.wait_time = duration
+		$Duration.start()
+	$Frequency.start()
+	new_shake()
+
+
+func new_shake():
+	var rand = Vector2()
+	rand.x = rand_range(-amplitude, amplitude)
+	rand.y = rand_range(-amplitude, amplitude)
+	$ShakeTween.interpolate_property(camera, "offset", camera.offset, rand, $Frequency.wait_time, TRANS, EASE)
+	$ShakeTween.start()
+
+
+func reset():
+	$ShakeTween.interpolate_property(camera, "offset", camera.offset, Vector2(), $Frequency.wait_time, TRANS, EASE)
+	$ShakeTween.start()
+
+
+func _on_Frequency_timeout():
+	new_shake()
+
+
+func _on_Duration_timeout():
+	reset()
+	$Frequency.stop()
