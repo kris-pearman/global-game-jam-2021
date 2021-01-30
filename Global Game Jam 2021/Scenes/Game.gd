e4be2ae267bab77e69c67951f1575b861ae79055
kris-pearman <57414402+kris-pearman@users.noreply.github.com>
2021-01-30 16:25:36 +0000

    Animated player
    Player is now animated and has footsteps!

--- Global Game Jam 2021/Scenes/Game.gd
+++ Global Game Jam 2021/Scenes/Game.gd
@@ -10,9 +10,13 @@ func _input(event):
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Intro.play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Intro_finished():
+	$Main.play()
