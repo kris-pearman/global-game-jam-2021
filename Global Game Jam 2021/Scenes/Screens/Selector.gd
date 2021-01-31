1c8ece659f85025ea40bc4a6299984c30d2d3c7d
kris-pearman <57414402+kris-pearman@users.noreply.github.com>
2021-01-31 10:35:23 +0000

    Title screen music implemented
    Title music plays and fades out as the screen does!

--- Global Game Jam 2021/Scenes/Screens/Selector.gd
+++ Global Game Jam 2021/Scenes/Screens/Selector.gd
@@ -1,8 +1,18 @@
 extends Sprite
 
+onready var tween_out_music = get_node("Tween")
+onready var active_stream = $TitleIntro
+export var transition_dur = 0.5
+export var transition_type = 1
+var cur_vol = 0
+
 var selection = 0
 var can_select = true
+var active = false
 
+func _ready():
+	$TitleIntro.play()
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _input(event):
 	if can_select == true:
@@ -14,12 +24,18 @@ func _input(event):
 			$UISelect.play()	
 		if event.is_action_pressed('ui_accept'):
 			$UIConfirm.play()
+			fade_out(active_stream)
 			can_select = false
+			active = true
 			
 			
 
 func _process(delta):
 	updatecursor()
+	if active == true:
+		#color[3] += 0.01
+		$ColorRect.color[3] += delta / 2
+		
 	
 	
 func change_selection(direction):
@@ -47,7 +63,17 @@ func activate_selection():
 		get_tree().quit()
 		
 
-
 func _on_UIConfirm_finished():
 	activate_selection()
 	
+
+func _on_TitleIntro_finished():
+	cur_vol = $TitleIntro.volume_db
+	$TitleIntro/TitleLoop.play()
+	$TitleIntro/TitleLoop.volume_db = cur_vol
+	active_stream = $TitleIntro/TitleLoop
+
+func fade_out(stream):
+#turns volume down over time
+	tween_out_music.interpolate_property(stream, "volume_db", -7, -80, transition_dur, transition_type, Tween.EASE_IN, 0)
+	tween_out_music.start()
