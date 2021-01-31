e7aa12f0bc5baaa35fec77cc8b86dea092a45508
Renrith <renrith@protonmail.com>
2021-01-31 12:02:30 +0000

    added drunk bar font and resized it

---
+++ Global Game Jam 2021/Scenes/DrunkBarLabel.gd
@@ -0,0 +1,7 @@
+extends Label
+
+
+func _ready():
+	var font: DynamicFont = preload("res://Assets/Fonts/font.tres")
+	add_font_override("font", font)
+	add_color_override("font_color", Color(0.35, 0.32, 0.38, 1))
