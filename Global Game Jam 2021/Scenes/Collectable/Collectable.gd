499a5edd95b2e7b1f6fe64307ae1aba61f5591e0
Renrith <renrith@protonmail.com>
2021-01-31 09:29:11 +0000

    added painting objects with sprites and their associated ids

--- Global Game Jam 2021/Scenes/Collectable/Collectable.gd
+++ Global Game Jam 2021/Scenes/Collectable/Collectable.gd
@@ -8,4 +8,8 @@ func on_body_entered(body):
 		queue_free()
 
 func _ready():
+	if id == 0:
+		$Sprite.texture = preload("res://Assets/Paintings/Chikoha.png")
+	elif id == 1:
+		$Sprite.texture = preload("res://Assets/Paintings/Swan Lake.png")
 	connect("body_entered", self, "on_body_entered")
